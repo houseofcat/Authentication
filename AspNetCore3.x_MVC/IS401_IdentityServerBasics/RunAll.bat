46df30c4aaa4a6889afd959d8fe7add55fa13733
Tristan (HouseCat) Hyams <cat@houseofcat.io>
2020-02-27 22:25:37 -0500

    Cleanup

--- AspNetCore3.x_MVC/IS401_IdentityServerBasics/RunAll.bat
+++ AspNetCore3.x_MVC/IS401_IdentityServerBasics/RunAll.bat
@@ -1,3 +1,3 @@
-start /d "." dotnet run --project .\IS401_IdentityServerBasics\IS401_IdentityServerBasics.csproj
-start /d "." dotnet run --project .\IS401_IdentityServerBasics\IS401_TestApi.csproj
-start /d "." dotnet run --project .\IS401_IdentityServerBasics\IS401_TestApiClient.csproj
+start /d "." dotnet run --project .\IS401_IdentityServerBasics\IS401_IdentityServerBasics.csproj -c Debug
+start /d "." dotnet run --project .\IS401_TestApi\IS401_TestApi.csproj -c Debug
+start /d "." dotnet run --project .\IS401_TestApiClient\IS401_TestApiClient.csproj -c Debug
--- AspNetCore3.x_MVC/IS402_IdentityServerMvcClient/RunAll.bat
+++
@@ -1,3 +0,0 @@
-start /d "." dotnet run --project .\IS401_IdentityServerBasics\IS401_IdentityServerBasics.csproj
-start /d "." dotnet run --project .\IS401_IdentityServerBasics\IS401_TestApi.csproj
-start /d "." dotnet run --project .\IS401_IdentityServerBasics\IS401_TestApiClient.csproj
