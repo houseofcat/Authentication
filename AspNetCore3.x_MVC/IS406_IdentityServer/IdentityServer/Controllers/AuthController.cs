f19d0db114dab29aaa70644b7abaef291f31acf8
Tristan (HouseCat) Hyams <cat@houseofcat.io>
2020-03-02 22:27:22 -0500

    Update AuthController.cs

--- AspNetCore3.x_MVC/IS406_IdentityServer/IdentityServer/Controllers/AuthController.cs
+++ AspNetCore3.x_MVC/IS406_IdentityServer/IdentityServer/Controllers/AuthController.cs
@@ -1,10 +1,19 @@
 ﻿using IdentityServer.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace IdentityServer.Controllers
 {
     public class AuthController : Controller
     {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AuthController(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
         [HttpGet]
         public IActionResult Login(string returnUrl)
         {
@@ -12,8 +21,28 @@ namespace IdentityServer.Controllers
         }
 
         [HttpPost]
-        public IActionResult Login(LoginViewModel model)
+        public async Task<IActionResult> LoginAsync(LoginViewModel model)
         {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, true, true);
+                if (result.Succeeded)
+                {
+
+                }
+                else if (result.IsLockedOut)
+                {
+
+                }
+                else if (result.IsNotAllowed)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
             return View();
         }
     }
