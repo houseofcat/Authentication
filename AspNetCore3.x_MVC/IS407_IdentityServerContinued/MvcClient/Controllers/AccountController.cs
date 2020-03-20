bad5a02446b4715c54172ff8e89d83a6d33dd8d5
Tristan (HouseCat) Hyams <cat@houseofcat.io>
2020-03-20 00:54:13 -0400

    Display tokens on profile page.

--- AspNetCore3.x_MVC/IS407_IdentityServerContinued/MvcClient/Controllers/AccountController.cs
+++ AspNetCore3.x_MVC/IS407_IdentityServerContinued/MvcClient/Controllers/AccountController.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MvcClient.ViewModels;
 using System.IdentityModel.Tokens.Jwt;
 using System.Threading.Tasks;
+using Utf8Json;
+using Newtonsoft;
+using Newtonsoft.Json;
 
 namespace MvcClient.Controllers
 {
@@ -16,32 +20,51 @@ namespace MvcClient.Controllers
         [Authorize]
         public async Task<IActionResult> ProfileAsync()
         {
-            var accessToken = await HttpContext.GetTokenAsync("access_token");
-            var idToken = await HttpContext.GetTokenAsync("id_token");
-            var refreshToken = await HttpContext.GetTokenAsync("refresh_token"); // if not explicity configured/asked for, should be null
-
-            if (!string.IsNullOrWhiteSpace(accessToken))
+            var viewModel = new ProfileViewModel
             {
-                var _accessToken = new JwtSecurityTokenHandler()
-                    .ReadJwtToken(accessToken);
+                AccessToken = await HttpContext.GetTokenAsync("access_token"),
+                IdToken = await HttpContext.GetTokenAsync("id_token"),
+                RefreshToken = await HttpContext.GetTokenAsync("refresh_token") // if not explicity configured/asked for, should be null
+            };
+
+            if (!string.IsNullOrWhiteSpace(viewModel.AccessToken))
+            {
+                // UTF8Json can't parse these tokens
+                //var token = new JwtSecurityTokenHandler().ReadJwtToken(viewModel.AccessToken);
+                //var jsonBytes = JsonSerializer.Serialize(token);
+                //viewModel.JsonAccessToken = JsonSerializer.PrettyPrint(jsonBytes);
+
+                // Newtonsoft
+                var token = new JwtSecurityTokenHandler().ReadJwtToken(viewModel.AccessToken);
+                viewModel.JsonAccessToken = JsonConvert.SerializeObject(token);
             } // Break Point Here To Look At _Token
             // Verify Claim {scope: Mvc.Scope} is available. 
 
-            if (!string.IsNullOrWhiteSpace(idToken))
+            if (!string.IsNullOrWhiteSpace(viewModel.IdToken))
             {
-                var _idToken = new JwtSecurityTokenHandler()
-                    .ReadJwtToken(idToken);
+                // UTF8Json can't parse these tokens
+                // var token = new JwtSecurityTokenHandler().ReadJwtToken(viewModel.IdToken);
+                // var jsonBytes = JsonSerializer.Serialize(token);
+                // viewModel.IdToken = JsonSerializer.PrettyPrint(jsonBytes);
+
+                var token = new JwtSecurityTokenHandler().ReadJwtToken(viewModel.IdToken);
+                viewModel.JsonIdToken = JsonConvert.SerializeObject(token);
             } // Break Point Here To Look At _Token
             // Verify Claim {ViewToken: access_token} is now seen. This is inside the Mvc.Scope created in IdentityServer4
             // This is available if the ID Token has added Claims To IdentityToken.
 
-            if (!string.IsNullOrWhiteSpace(refreshToken))
+            if (!string.IsNullOrWhiteSpace(viewModel.RefreshToken))
             {
-                var _refreshToken = new JwtSecurityTokenHandler()
-                    .ReadJwtToken(refreshToken);
+                // UTF8Json can't parse these tokens
+                //var token = new JwtSecurityTokenHandler().ReadJwtToken(viewModel.RefreshToken);
+                //var jsonBytes = JsonSerializer.Serialize(token);
+                //viewModel.RefreshToken = JsonSerializer.PrettyPrint(jsonBytes);
+
+                var token = new JwtSecurityTokenHandler().ReadJwtToken(viewModel.RefreshToken);
+                viewModel.JsonRefreshToken = JsonConvert.SerializeObject(token);
             } // Break Point Here To Look At _Token
 
-            return View();
+            return View(viewModel);
         }
     }
 }
--- AspNetCore3.x_MVC/IS407_IdentityServerContinued/MvcClient/Models/ViewModels/ProfileViewModel.cs
+++ AspNetCore3.x_MVC/IS407_IdentityServerContinued/MvcClient/Models/ViewModels/ProfileViewModel.cs
@@ -2,5 +2,11 @@
 {
     public class ProfileViewModel
     {
+        public string AccessToken { get; set; }
+        public string JsonAccessToken { get; set; }
+        public string IdToken { get; set; }
+        public string JsonIdToken { get; set; }
+        public string RefreshToken { get; set; }
+        public string JsonRefreshToken { get; set; }
     }
 }
